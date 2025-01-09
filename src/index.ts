3479b0932e09449bcaea11d0dce72c0949c68f7b
Amy <ampyaephyonaing@gmail.com>
2025-01-09 14:32:29 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { json } from 'body-parser';  
+import express from 'express';
+import axios from 'axios';
+const app = express();
+
+app.use(json());   
+
+const router = express.Router();
+
+router.get('/test' ,(req, res) => {
+    console.log('API_KEY', process.env.API_KEY,process.env.API_SECRET)
+    axios.get(`${process.env.API_KEY}}/api/method/frappe.auth.get_logged_user`, {
+        headers: {
+            "Authorization" : `token ${process.env.API_KEY}:${process.env.API_SECRET}`
+        }
+    }).then(({data}) => {
+        res.send(data)
+    }).catch(error => {
+        // console.log(error);
+    }) 
+});
+
+app.use(router);
+
+app.listen(3000, () => {
+    console.log('APP is starting....');
+});
